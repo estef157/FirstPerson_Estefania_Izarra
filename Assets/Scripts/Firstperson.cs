4f02e532b5bf2dc97d06c22b951cb6134702fe83
estef157 <estefaniaizarra8@gmail.com>
2024-10-21 17:45:41 +0200

    21/10

---
+++ Assets/Scripts/Firstperson.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firstperson : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

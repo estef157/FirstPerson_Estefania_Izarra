45cafe491a4d200e3015054e094ae8953aef8254
estef157 <estefaniaizarra8@gmail.com>
2024-10-23 21:13:44 +0200

    23/10

--- Assets/Scripts/Firstperson.cs
+++ Assets/Scripts/Firstperson.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Firstperson : MonoBehaviour
+
 {
+    [SerializeField] private float velocidadMovimiento;
+    CharacterController characterController;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        //Vector3 movimiento  = new Vector3 (h, 0, v);
+        Vector2 input  = new Vector2 (h, v).normalized;
+
+        characterController.Move(input * velocidadMovimiento * Time.deltaTime);
     }
 }

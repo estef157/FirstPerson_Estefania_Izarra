258eb04d03094ae27eabc155cae973ed28053078
estef157 <estefaniaizarra8@gmail.com>
2024-10-24 21:11:26 +0200

    24/10

--- Assets/Scripts/Firstperson.cs
+++ Assets/Scripts/Firstperson.cs
@@ -7,11 +7,18 @@ public class Firstperson : MonoBehaviour
 {
     [SerializeField] private float velocidadMovimiento;
     CharacterController characterController;
-   
+    private Camera cam;
+    [SerializeField] private float smoothing;
+    private float velocidadRotacion;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<CharacterController>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -21,6 +28,24 @@ public class Firstperson : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
         //Vector3 movimiento  = new Vector3 (h, 0, v);
         Vector2 input  = new Vector2 (h, v).normalized;
+
+        if  (input.sqrMagnitude > 0)
+        {
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloRotacion, ref velocidadRotacion, smoothing);
+         transform.eulerAngles = new Vector3(0, anguloSuave, 0);
+
+            Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
+            characterController.Move(movimiento * 5 * Time.deltaTime);
+            
+        }
+
+
+
+
+
+
+
 
         characterController.Move(input * velocidadMovimiento * Time.deltaTime);
     }
